903ead660fbb655382550627e38001e7553041b0
TheTerrierKnight <Rdendebera@gmail.com>
2024-02-28 17:20:21 -0600

    Files
    Organization

--- Atmos (Proton)/AtmosProton.py
+++ Atmos (Proton)/AtmosProton.py
@@ -8,23 +8,40 @@ import turtle
 import random
 import time
 
-# GLOBAL CONFIGS
+# GLOBAL CONFIGS (Use 0 or 1)
 UseGlobalPrefix = 0
 UseGlobalPath = 0
 RunCMD = 1 # Runs the Code
-NoticeTest = 1
+NoticeTest = 1 # Test for AEN Manager
 
 
-# Alert/Error/Notice Manager
- #Notices
+# Alert/Error/Notice Manager (AEN Manager)
+
+#Notices
 if NoticeTest == 1:
- print("Notice Test is Enabled.")
+ print("Notice: NoticeTest is Enabled.")
 
+#Errors
+
+#Alerts
+if RunCMD != 1:
+ print("Error: RunCMD is not 1, Please Check RunCMD.")
+
+# End of AEN Manager
+
+
+
+
+
+
+
+
+# MAIN CODE
 
 # Atmos-CMD (Proton) 
 
 while RunCMD == True:
-   command = input("Enter a Command: ")
+   command = input("Atmos-CMD: ")
    if command == (AdminPrefix + "RunTest"):
     print("Running...")
     time.sleep(1)
