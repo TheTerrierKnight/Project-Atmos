66cbaba2b68b79dbd8e7ac44f8051f2191af73e9
TheTerrierKnight <Rdendebera@gmail.com>
2024-02-28 17:13:50 -0600

    Update 1
    Update 1

--- AtmosProton.py
+++ AtmosProton.py
@@ -1,16 +1,36 @@
-# Start Up CMD Line
- # nothing here yet :(
-
-
+# Start Up CMD Line (DO NOT TOUCH)
+GlobalPath = [36/34/67/44/22/33/44/55] # Does Nothing yet :(
+GlobalPrefix = "AtmosProton.py" # Does nothing yet :(
+AdminPrefix = "!"
+print("Preloading Finished! Setting Up Configs and Starting Atmos-CMD")
 # Inport All needed Stuff
 import turtle
 import random
 import time
 
+# GLOBAL CONFIGS
+UseGlobalPrefix = 0
+UseGlobalPath = 0
+RunCMD = 1 # Runs the Code
+NoticeTest = 1
+
+
+# Alert/Error/Notice Manager
+ #Notices
+if NoticeTest == 1:
+ print("Notice Test is Enabled.")
+
+
 # Atmos-CMD (Proton) 
 
-while True:
+while RunCMD == True:
    command = input("Enter a Command: ")
+   if command == (AdminPrefix + "RunTest"):
+    print("Running...")
+    time.sleep(1)
+    print("Wohoo! The Prefix Works!")
+   if command == ("Help"):
+    print("version-1,version-2,forward,backward,left,right,color select,exit")
    if command == "version-1":
     print("You are Running AtmosProtonCMD-V1")
    if command == "version-2":
@@ -32,3 +52,5 @@ while True:
     turtle.color(color)
    if command == "exit":
     turtle.done()
+   
+    
